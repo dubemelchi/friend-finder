3e1d1fc3044ddf7254902bdd028483bb47506d5c
dubemelchi <dubemelchi@gmail.com>
2019-08-03 06:49:11 -0400

    dependencies, ports, route file and listener

---
+++ server.js
@@ -0,0 +1,21 @@
+// dependencies
+var express = require("express");
+
+// tells node we are creating an express server
+var app = express();
+
+// seting an initial port
+var PORT = process.env.PORT || 9000;
+
+// data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// route files 
+require("./app/routing/html-routes")(app);
+require("./app/routing/api-routes");
+
+// listener
+app.listen(PORT, function() {
+ console.log("App listening on PORT: " + PORT);
+})
