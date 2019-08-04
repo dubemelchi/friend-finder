124c92c313d7d59b493aebb577fbe1979535af9f
dubemelchi <dubemelchi@gmail.com>
2019-08-04 01:05:51 -0400

    possible friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,26 @@
-//
+// possible friends
+var friends = [
+ {
+  name: "scarlette johansen",
+  photo: "",
+  scores: ["3", "1", "2", "5", "4","4", "2", "1", "3", "3"]
+ },
+  {
+  name: "Amanda Bach",
+  photo: "",
+  scores: ["2", "5", "1", "3", "2","4", "1", "2", "1", "1"]
+ },
+  {
+  name: "Mikki williams",
+  photo: "",
+  scores: ["1", "2", "3", "4", "5","5", "4", "3", "2", "1"]
+ },
+
+ {
+  name: "Christi Sta",
+  photo: "",
+  scores: ["5", "4", "3", "2", "1","1", "2", "3", "4", "5"]
+ },
+];
+
+module.exports = friends;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -10,6 +10,35 @@ module.exports = function(app) {
  });
 
  app.post("/api/friends", function(req, res) {
-  
- })
-}
+  var newFriend = req.body;
+  var myScore = newFriend.scores;
+  var score = [];
+  for (var x = 0; x < myScore.length; x++) {
+   score.push(parseInt(newFriend.scores[x]));
+
+  };
+
+  newFriend.scores = score;
+  var total = 0;
+  var issaMatch = 100;
+  var index = -1;
+
+  for(var j = 0; j < friends.length; j++) {
+   total = 0;
+   for(var i = 0; i < myScore.length; i++) {
+    var dif = Math.abs(myScore[i] - friends[j].scores[i]);
+    total += dif;
+   };
+
+   if(total < issaMatch) {
+    issaMatch = total;
+    index = j;
+   };
+  };
+
+  friend.push(newFriend);
+  console.log(newFriend);
+  res.json(friends[index]);
+  console.log(friends[index]);
+ });
+};
