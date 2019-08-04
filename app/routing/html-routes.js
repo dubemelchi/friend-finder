5c9c8e1ae251e9e49dcfafa71f0d86792caa25c3
dubemelchi <dubemelchi@gmail.com>
2019-08-04 13:24:18 -0400

    re- to require

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -4,7 +4,7 @@ var path = require("path");
 // routing
 module.exports = function(app) {
 
- app.get(function(req, res) {
+ app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname + "../public/survey.html"));
  });
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // try this again
 // dependencies
 var express = require("express");
-var bodyParser = re("body-parser");
+var bodyParser = require("body-parser");
 var path = require("path");
 
 // tells node we are creating an express server
@@ -12,9 +12,9 @@ var PORT = process.env.PORT || 3000;
 
 // data parsing
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(express.json({ type: "application/vnd.api+json"}));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static("app/public"));
 
 // route files 
